398abfcc857f5326389e74f99b022d0ae607f826
halimkun <ffaisalhalim@gmail.com>
2022-12-26 23:08:19 +0700

    update home page

--- pages/home.py
+++ pages/home.py
@@ -1,8 +1,20 @@
 import dash
 from dash import html, dcc
+from components import text as tc
 
 dash.register_page(__name__, path='/', order=1)
 
-layout = html.Div(children=[
-    html.H1(children='Halaman Home', className='text-3xl font-bold'),
-])
+# flex 2 columns
+layout = html.Div([
+    html.Div([
+        html.Img(src=dash.get_asset_url('i.png'), className='w-[80%]'),
+    ], className='flex flex-col justify-center items-center w-full md:w-[50%]'),
+    html.Div([
+        html.Div([
+            html.Div([tc.text_4xl('Recomendation App.')], className='mb-3'),
+            tc.text_base('Aplikasi rekomendasi yang memanfaatkan metode klasisifikasi dengan algoritma Decision Tree.'),
+            html.Div([], className='mb-3'),
+            tc.text_base('Recomendation App ini diperuntukan untuk membantu BKK SMK dalam menentukan rekomendasi perusahaan yang sesuai dengan minat dan bakat siswa.')
+        ], className='w-[80%]'),
+    ], className='flex flex-col justify-center items-center w-full md:w-[50%]'),
+], className='flex flex-col md:flex-row w-full justify-center items-center mt-10 p-3')
