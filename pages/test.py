9c5a3815d78ccda4a7ddfb54e6e58ea3313b73a2
halimkun <ffaisalhalim@gmail.com>
2023-01-10 08:38:12 +0700

    Create test.py

---
+++ pages/test.py
@@ -0,0 +1,14 @@
+# import dash
+# from dash import html, dcc
+# import pdfkit
+
+# # Register the page
+# dash.register_page(__name__, path='/test', order=5)
+
+# # Define the layout
+# layout = html.Div(children=[
+#     html.H1(children='Test Page', className='text-3xl font-bold'),
+# ])
+
+# layout = pdfkit.from_string(layout, False)
+
